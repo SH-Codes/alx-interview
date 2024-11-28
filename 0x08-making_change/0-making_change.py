8c3dc8c50ab56dc1c22cf62a06503ad243dceda3
SH-Codes <wldrms@outlook.com>
2024-11-28 22:20:04 +0200

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 
+""" Contains makeChange function"""
+
+
 def makeChange(coins, total):
     """
-    Returns the fewest number of coins needed to meet total.
-    If total is 0 or less, return 0.
-    If total cannot be met by any number of coins you have, return -1.
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
     """
-    if total <= 0:
-        return 0
     if not coins or coins is None:
         return -1
-
-    # Initialize a DP array to store the minimum coins needed for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make total 0
-
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
