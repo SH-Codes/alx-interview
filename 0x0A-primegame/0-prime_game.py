e38e2882997f9771b22bb2f16d28a942c700809c
SH-Codes <wldrms@outlook.com>
2024-12-12 20:17:06 +0200

    0x0A-primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,35 +1,29 @@
+#!/usr/bin/python3
+"""Program that performs prime game"""
+
+
 def isWinner(x, nums):
-    def sieve(n):
-        primes = []
-        is_prime = [True] * (n + 1)
-        for p in range(2, n + 1):
-            if is_prime[p]:
-                primes.append(p)
-                for multiple in range(p * p, n + 1, p):
-                    is_prime[multiple] = False
-        return primes
-
-    def play_game(n):
-        primes = sieve(n)
-        moves = 0
-        while primes:
-            prime = primes.pop(0)
-            moves += 1
-            primes = [p for p in primes if p % prime != 0]
-        return moves % 2 == 1
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if play_game(n):
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """Function that performs prime game"""
+    if not nums or x < 1:
         return None
+    n = max(nums)
+    fltr = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not fltr[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            fltr[j] = False
+    fltr[0] = fltr[1] = False
+    c = 0
+    for i in range(len(fltr)):
+        if fltr[i]:
+            c += 1
+        fltr[i] = c
+    plyr1 = 0
+    for n in nums:
+        plyr1 += fltr[n] % 2 == 1
+    if plyr1 * 2 == len(nums):
+        return None
+    if plyr1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
